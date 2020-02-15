ba27d6bca27259b5cc520ff418c9a8823d98d793
fmendoz7 <francissamuelmendoza7@gmail.com>
2020-02-15 04:00:26 -0700

    PYTHON3: Finished Binary Heap code & principles

--- Data Structures/Python3/BinaryHeap.py
+++ Data Structures/Python3/BinaryHeap.py
@@ -16,7 +16,7 @@
             30                                 40 50   100 40
 
     COMMON HEAP OPERATIONS
-        1. getMini()
+        1. getMin()/getMax()
             > Returns root element of the min heap
             > Time Complexity: O(1)
 
@@ -25,8 +25,8 @@
             > Time Complexity: O(log(n))
                 i. Needs to use heapify with each iteration of removing root
 
-        3. decreaseKey()
-            > Decreases value of key
+        3. decreaseKey()/increaseKey()
+            > Decreases value of key (minHeap)
             > Time Complexity: O(log(n))
 
         4. insert()
@@ -39,15 +39,45 @@
             > Time Complexity: O(log(n))
 """
 
+#INDEX VALUE is different from key. Just represents nth element 
+#KEY VALUE references ACTUAL VALUE
+
 class MinHeap:
 
+    #Constructor for the heap
     def __init__(self):
         self.heap = []
 
+    #Get indice of the parent
     def parent(self, i):
         return (i-1)/2
 
+    #Insert new key, 'k'
     def insertKey(self, k):
         heappush(self.heap, k)
+
+    #Method to REMOVE minimum element from min heap (root)
+    def extractMin(self):
+        return heappop(self.heap)
+            
+    #Decrease value of key to new value (smaller than heap[i])
+    def decreaseKey(self, i, new_val):
+        self.heap[i] = new_val
+
+        while(i != 0 and self.heap[self.parent(i)] > self.heap[i]):
+            #Swap heap[i] and heap[parent(i)] for minHeap
+            self.heap[i] = self.heap[self.parent(i)]
+            self.heap[self.parent(i)] = self.heap[i]
+
+    #Delete node with given key 
+    def deleteKey(self, i): 
+            self.decreaseKey(i, float("-inf")) 
+                #Essentially, artificially devalued the key to an insanely low number
+            self.extractMin() 
+                #Basically deleted from there
+
+    #Trivial method *assuming you sorted properly* to IDENTIFY, where you just get ROOT (index 0)
+    def getMin(self):
+        return self.heap[0]
 
     
